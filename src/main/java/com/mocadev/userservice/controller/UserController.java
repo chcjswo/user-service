6662df4564925f7f6585f2103fc8e8ce3258bce4
tony <chcjswo@gmail.com>
2021-07-20 23:28:23 +0900

    health-check 적용

---
+++ src/main/java/com/mocadev/userservice/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.mocadev.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2021-07-20
+ **/
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+	@GetMapping("/health-check")
+	public String status() {
+		return "It's Working in User Service";
+	}
+
+}
