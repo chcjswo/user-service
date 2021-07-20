7220e63ca70e9dd1c1da9dead04fa6ad22ddf896
tony <chcjswo@gmail.com>
2021-07-21 00:33:26 +0900

    Environment 사용

--- src/main/java/com/mocadev/userservice/controller/UserController.java
+++ src/main/java/com/mocadev/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mocadev.userservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+	private final Environment env;
+
 	@GetMapping("/health-check")
 	public String status() {
 		return "It's Working in User Service";
+	}
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return env.getProperty("greeting.message");
 	}
 
 }
