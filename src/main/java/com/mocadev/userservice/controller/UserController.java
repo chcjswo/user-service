09ba96ef40ff0e2856506c0ea83a2cba9e558673
tony <chcjswo@gmail.com>
2021-07-22 22:20:06 +0900

    유저생성 반환값 변경
    - String message -> ResponseEntity

--- src/main/java/com/mocadev/userservice/controller/UserController.java
+++ src/main/java/com/mocadev/userservice/controller/UserController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,14 +48,14 @@ public class UserController {
 	}
 
 	@PostMapping("/users")
-	public String createUser(@RequestBody RequestUser requestUser) {
+	public ResponseEntity createUser(@RequestBody RequestUser requestUser) {
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserDto userDto = mapper.map(requestUser, UserDto.class);
 
 		userService.createUser(userDto);
 
-		return "Created User method is called";
+		return new ResponseEntity(HttpStatus.CREATED);
 	}
 
 }
