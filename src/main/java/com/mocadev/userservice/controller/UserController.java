561a03a66ae4f6830f237c0d061f20abf565266f
tony <chcjswo@gmail.com>
2021-07-23 00:45:53 +0900

    유저 조회 추가

--- src/main/java/com/mocadev/userservice/controller/UserController.java
+++ src/main/java/com/mocadev/userservice/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.mocadev.userservice.controller;
 
 import com.mocadev.userservice.dto.UserDto;
+import com.mocadev.userservice.repository.UserEntity;
 import com.mocadev.userservice.service.UserService;
 import com.mocadev.userservice.vo.Greeting;
 import com.mocadev.userservice.vo.RequestUser;
 import com.mocadev.userservice.vo.ResponseUser;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -12,6 +15,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +61,27 @@ public class UserController {
 		ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+	}
+
+	@GetMapping("/users")
+	public ResponseEntity<List<ResponseUser>> getUsers() {
+		Iterable<UserEntity> users = userService.getUserByAll();
+		List<ResponseUser> result = new ArrayList<>();
+		ModelMapper mapper = new ModelMapper();
+
+		users.forEach(v -> {
+			result.add(mapper.map(v, ResponseUser.class));
+		});
+
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
+
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<ResponseUser> getUser(@PathVariable String userId) {
+		UserDto userDto = userService.getUserByUserId(userId);
+		ModelMapper mapper = new ModelMapper();
+		ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+		return ResponseEntity.status(HttpStatus.OK).body(responseUser);
 	}
 
 }
